374c7a5aabebc3d3f9195c1b81c7074db3ec4001
sauravk7077 <sauravk7077@gmail.com>
2020-07-16 11:31:04 +0530

    added views

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const socket = require('socket.io');
+const session = require('express-session');
+const fmsg = require('./others/messages'); //format message
 
 const app = express();
 
@@ -11,16 +13,45 @@ const server = app.listen(PORT,()=>{
 
 let io = socket(server);
 
+app.set('view engine', 'ejs');
+
 app.use(express.static('src'));
+app.use(express.urlencoded({extended:true}));
+app.use(session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false
+}));
 
 
 io.on('connection', (socket)=>{
     console.log('Connection is made');
-
+    socket.emit('message', fmsg('ChatBoom', "Welcome to ChatBoom."));
+    socket.broadcast.emit('message', fmsg('ChatBoom', `A user has joined the room.`));
     socket.on('chat', (data)=>{
-        socket.emit('chat', data);
+        socket.broadcast.emit('chat', fmsg('User', data));
     });
     socket.on('disconnect', ()=>{
-        console.log('User disconnected');
-    })
+        socket.broadcast.emit('message', fmsg("ChatBoom", `A user has disconnected`));
+    });
+});
+
+
+
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+app.get('/chat', (req,res)=>{
+    if(req.session.username){
+        res.render('chat');
+    }else{
+        res.redirect('/');
+    }
+});
+
+app.post('/chat', (req, res)=>{
+    req.session.username = req.body.username;
+    req.session.room = req.body.rooms;
+    res.render('chat');
 });
--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-// Make connection
+const {username, rooms} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
 
+console.log(username, rooms);
+
+// Make connection
 window.socket = io.connect('http://localhost:3000');
 
-var sendBtn = document.getElementById('send');
-var dataInput = document.getElementById('text');
+let sendBtn = document.getElementById('send');
+let dataInput = document.getElementById('input');
+let chatsDiv = document.getElementById('chats');
 
 sendBtn.addEventListener('click', ()=>{
     socket.emit('chat', dataInput.value);
     dataInput.value = '';
+    dataInput.focus();
 });
 
 
+socket.on('message', (data)=>{
+    displayData(data);
+});
 socket.on('chat', (data)=>{
-    document.getElementById('data').innerText += '\n' +data;
-});
+    displayData(data);
+})
+
+
+function displayData(object) {
+    let msg = document.createElement('div');
+    let meta = document.createElement('p');
+    let message = document.createElement('p');
+    meta.innerText = object.username + " - " + object.time;
+    message.innerText = object.text;
+    meta.className = "meta";
+    message.className = "text";
+    msg.className = "message";
+    msg.append(meta, message);
+    chatsDiv.append(msg);
+}
